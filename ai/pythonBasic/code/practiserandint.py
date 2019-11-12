06c5335ccb7110ee6ca8859d86269d2f4c65af43
XueYuSky <xueyusky@gmail.com>
2019-11-12 12:05:46 +0800

    Deployed c5d58a5 with MkDocs version: 1.0.4

---
+++ ai/pythonBasic/code/practiserandint.py
@@ -0,0 +1,7 @@
+from random import randint
+basic = 10
+var1 = randint(10,100) + basic
+var2 = randint(10,100) + basic
+if __name__ == "__main__":
+    print("第一个随机数：",var1)
+    print("第二个随机数：",var2)
